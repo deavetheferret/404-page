c4176cba1a1550ecb3dd2131251734e7a8f33ca9
François Devantay <francois.devantay@studentfr.ch>
2022-10-06 00:22:17 +0200

    add other websites to start my website

---
+++ src/websites/exam-imd22/js/index.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".show-all");document.querySelector(".hidden-tmp"),document.querySelector(".hidden-tmp-2");e.addEventListener("click",(function(){document.querySelector(".hidden-tmp").classList.toggle("show"),document.querySelector(".hidden-tmp-2").classList.toggle("show")}));
+//# sourceMappingURL=index.js.map
